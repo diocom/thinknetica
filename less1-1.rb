c5f2037f5d23ca76c72e64d2f8f1ccdffd5e57c1
diocom <diocomm@mail.ru>
2018-03-13 23:02:40 +0300

    Lesson1

---
+++ less1-1.rb
@@ -0,0 +1,18 @@
+# lesson 1. ex. 1
+#programm for calculating of ideal weight
+puts "Hello, input you first name"              #запрос ввода имени 
+you_name = gets.chomp				#ждем ввода имени
+puts "Hi #{you_name}! Input you height in cm"
+height = gets.chomp.to_i			#ждем рост
+weight = height-110				#вычисляем вес
+if height > 47&&height < 273		        #проверяем введенный рост
+  if weight < 0
+   puts "You weight is optimal:)"
+  else
+   puts "You weight is NOT optimal!!!"
+  end
+else
+  puts "Error! Wrong height #{height.to_s}"
+end
+
+
